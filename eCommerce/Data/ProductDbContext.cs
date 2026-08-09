e00209a8f8e90deb08720b302c995560840834d4
Zac Kimball <z.kimball85@gmail.com>
2026-08-09 15:35:53 -0700

    Add ProductDbContext for Product entity management
    Introduced ProductDbContext in eCommerce.Data, inheriting from DbContext, with a DbSet<Product> for database operations. Added required using directives for eCommerce.Models and Microsoft.EntityFrameworkCore.

---
+++ eCommerce/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // Entities to be added to the database context
+    public DbSet<Product> Products { get; set; }
+}
