f9d706b0c8048c6d704d735f0cc639a793511113
Zac Kimball <z.kimball85@gmail.com>
2026-08-09 20:12:37 -0700

    Make Product listing async, improve display and navigation
    - Updated ProductController Index action to be asynchronous and fetch products via EF Core.
    - Annotated Product.Price with [DataType.Currency] for better formatting.
    - Fixed _Layout.cshtml navigation link to use "Product" controller.
    - Added/updated Index.cshtml to show product table and create button.

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.Data;
 using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Controllers;
 
@@ -13,9 +14,11 @@ public class ProductController : Controller
         _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        // Retrieve the list of products from the database
+        List<Product> allProducts = await _context.Products.ToListAsync();
+        return View(allProducts);
     }
 
     [HttpGet]
