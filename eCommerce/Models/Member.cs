ce68add553b5b977dd6c2a24fca74342e548b00b
Zac Kimball <z.kimball85@gmail.com>
2026-08-21 20:23:32 -0700

    Restrict Username/Password, add DateOfBirth to Member
    - Reduced Username max length to 25 and enforced alphanumeric input
    - Set Password length requirement to 8-50 characters
    - Added DateOfBirth (DateOnly) property to Member class

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -15,15 +15,18 @@ public class Member
     // The username of the member. 
     // AlphaNumeric characters only, no spaces or special characters allowed.
     [Required]
-    [StringLength(100)]
+    [StringLength(25)]
+    [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain alphanumeric characters.")]
     public required string Username { get; set; }
 
     // The email address of the member.
     public required string Email { get; set; }
 
     // The password of the member.
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters.")]
     public required string Password { get; set; }
 
     // The date of birth of the member.
+
     public DateOnly DateOfBirth { get; set; }
 }
