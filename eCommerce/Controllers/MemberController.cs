8731f5893355788a8311f691f988f096cfa3f52e
Zac Kimball <z.kimball85@gmail.com>
2026-08-21 21:14:08 -0700

    Add MemberController and RegistrationViewModel for signup
    Introduced MemberController with Register action to serve registration view. Added RegistrationViewModel with validation for user registration fields. Member class unchanged except for addition of RegistrationViewModel.

---
+++ eCommerce/Controllers/MemberController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers;
+
+public class MemberController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -30,3 +30,39 @@ public class Member
 
     public DateOnly DateOfBirth { get; set; }
 }
+
+
+/// <summary>
+/// Represents the view model for member registration, 
+/// containing the necessary fields for user input during the registration process.
+/// </summary>
+public class RegistrationViewModel
+{
+
+    [Required]
+    [StringLength(25)]
+    [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain alphanumeric characters.")]
+    public required string Username { get; set; }
+
+
+    [Required]
+    [EmailAddress]
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+
+    [Required]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters.")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Required]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+}
